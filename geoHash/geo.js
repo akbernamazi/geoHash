cb6637d38d7570557683b930d3a5ca2612b0d113
akbernamazi <akbarnamazi123@gmail.com>
2020-10-27 15:03:42 +0530

    Updating API

--- geoHash/geo.js
+++ geoHash/geo.js
@@ -1,10 +1,7 @@
 var geohash = require("ngeohash");
 
 exports.getGeoHash = (req, res) => {
-  console.log(req.query);
-  return res
-    .status(200)
-    .json({
-      geohashcode: geohash.encode(req.query.latitude, req.query.longitude),
-    });
+  return res.status(200).json({
+    geohashcode: geohash.encode(req.query.latitude, req.query.longitude),
+  });
 };
